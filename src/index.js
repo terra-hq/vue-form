b59eb724a480eb79e514b912e1b87020b4a57c01
Nerea Fontecha <nerea@landt.co>
2023-11-22 12:44:15 +0100

    components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+import FormGroup from "./components/FormGroup.vue"
+import Label from "./components/Label.vue"
+import TextField from "./components/TextField.vue"
+import Checkbox from "./components/Checkbox.vue"
+import ErrorAndHint from "./components/ErrorAndHint.vue"
+import TextArea from "./components/TextArea.vue"
 import TextField from "./components/TextField.vue"
 
 export default {
     install: (app, options) => {
         app.component("TextField", TextField)
+        app.component("FormGroup", FormGroup)
+        app.component("Label", Label)
+        app.component("Checkbox", Checkbox)
+        app.component("TextField", TextField)
+        app.component("ErrorAndHint", ErrorAndHint)
+        app.component("TextArea", TextArea)
     },
 }
-
