88efeee76da0bbcea3e99a457bcb28c7d24bd9bd
Julieta Fredes <julietal.fredes@gmail.com>
2023-11-23 10:49:19 +0100

    use all components

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import TextField from "./components/TextField.vue"
 import Checkbox from "./components/Checkbox.vue"
 import ErrorAndHint from "./components/ErrorAndHint.vue"
 import TextArea from "./components/TextArea.vue"
-import TextField from "./components/TextField.vue"
 
 export default {
     install: (app, options) => {
-        app.component("TextField", TextField)
         app.component("FormGroup", FormGroup)
         app.component("Label", Label)
-        app.component("Checkbox", Checkbox)
         app.component("TextField", TextField)
+        app.component("Checkbox", Checkbox)
         app.component("ErrorAndHint", ErrorAndHint)
         app.component("TextArea", TextArea)
     },
