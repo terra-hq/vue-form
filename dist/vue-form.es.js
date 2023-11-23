732b0a75ae3bc3808d7bad98c021b434a3c0d04f
Nerea Fontecha <nerea@landt.co>
2023-11-23 17:06:47 +0100

    inde

--- dist/vue-form.es.js
+++ dist/vue-form.es.js
@@ -1,5 +1,31 @@
-import { ref as i, watch as o, openBlock as c, createElementBlock as s, normalizeClass as u, createElementVNode as m } from "vue";
-const h = ["type", "value", "id", "maxlength", "placeholder", "required"], p = {
+import { openBlock as u, createElementBlock as s, normalizeClass as a, renderSlot as C, toDisplayString as c, createCommentVNode as f, ref as g, watch as y, createElementVNode as m, withDirectives as x, vModelText as S } from "vue";
+const v = {
+  __name: "FormGroup",
+  props: {
+    formClass: String
+  },
+  setup(e) {
+    return (t, n) => (u(), s("div", {
+      class: a([e.formClass])
+    }, [
+      C(t.$slots, "default")
+    ], 2));
+  }
+}, V = ["for"], p = {
+  __name: "Label",
+  props: {
+    textLabel: String,
+    forId: String,
+    labelClass: String
+  },
+  setup(e) {
+    return (t, n) => e.textLabel ? (u(), s("label", {
+      key: 0,
+      for: e.forId,
+      class: a(e.labelClass)
+    }, c(e.textLabel), 11, V)) : f("", !0);
+  }
+}, $ = ["type", "value", "id", "maxlength", "placeholder", "required"], b = {
   __name: "TextField",
   props: {
     type: {
@@ -26,35 +52,137 @@ const h = ["type", "value", "id", "maxlength", "placeholder", "required"], p = {
       default: !1
     }
   },
-  setup(e, { emit: a }) {
-    const n = e, l = i(n.modelValue), r = a, d = (t) => {
-      l.value = t.target.value, r("update:modelValue", l.value);
+  setup(e, { emit: t }) {
+    const n = e, l = g(n.modelValue), o = t, i = (r) => {
+      l.value = r.target.value, o("update:modelValue", l.value);
     };
-    return o(
+    return y(
       () => n.modelValue,
-      (t) => {
-        l.value = t;
+      (r) => {
+        l.value = r;
       }
-    ), (t, f) => (c(), s("div", {
-      class: u([e.inputClass, { [`${e.inputClass}--error`]: e.error }])
+    ), (r, d) => (u(), s("div", {
+      class: a([e.inputClass, { [`${e.inputClass}--error`]: e.error }])
     }, [
       m("input", {
         type: e.type,
         value: l.value,
         id: e.id,
-        class: u([e.inputClass + "__item"]),
-        onInput: d,
+        class: a([e.inputClass + "__item"]),
+        onInput: i,
         maxlength: e.maxlength,
         placeholder: e.placeholder,
         required: e.required
-      }, null, 42, h)
+      }, null, 42, $)
     ], 2));
   }
-}, x = {
-  install: (e, a) => {
-    e.component("TextField", p);
+}, k = ["id", "required", "checked", "value"], q = {
+  __name: "Checkbox",
+  props: {
+    id: String,
+    inputClass: String,
+    modelValue: Boolean,
+    error: {
+      type: Boolean,
+      default: !1
+    },
+    required: {
+      type: Boolean,
+      default: !1
+    }
+  },
+  emits: ["update:modelValue"],
+  setup(e) {
+    return (t, n) => (u(), s("div", {
+      class: a([e.inputClass, { [`${e.inputClass}--error`]: e.error }])
+    }, [
+      m("input", {
+        id: e.id,
+        type: "checkbox",
+        class: a([e.inputClass + "__item"]),
+        required: e.required,
+        checked: e.modelValue,
+        value: e.modelValue,
+        onChange: n[0] || (n[0] = (l) => t.$emit("update:modelValue", !e.modelValue))
+      }, null, 42, k)
+    ], 2));
+  }
+}, B = {
+  __name: "ErrorAndHint",
+  props: {
+    errorMessage: {
+      type: String,
+      default: null
+    },
+    errorClass: {
+      type: String,
+      default: null
+    },
+    hintMessage: {
+      type: String,
+      default: null
+    },
+    hintClass: {
+      type: String,
+      default: null
+    }
+  },
+  setup(e) {
+    return (t, n) => e.errorMessage ? (u(), s("p", {
+      key: 0,
+      class: a(e.errorClass)
+    }, c(e.errorMessage), 3)) : e.hintMessage ? (u(), s("p", {
+      key: 1,
+      class: a(e.hintClass)
+    }, c(e.hintMessage), 3)) : f("", !0);
+  }
+}, M = ["id", "placeholder", "rows"], w = {
+  __name: "TextArea",
+  props: {
+    modelValue: String,
+    id: String,
+    inputClass: String,
+    rows: {
+      type: String,
+      default: "3"
+    },
+    placeholder: {
+      type: String,
+      default: null
+    },
+    required: {
+      type: Boolean,
+      default: !1
+    },
+    error: {
+      type: Boolean,
+      default: !1
+    }
+  },
+  setup(e, { emit: t }) {
+    const l = g(e.modelValue), o = t, i = (r) => {
+      l.value = r.target.value, o("update:modelValue", l.value);
+    };
+    return (r, d) => (u(), s("div", {
+      class: a([e.inputClass, { [`${e.inputClass}--error`]: e.error }])
+    }, [
+      x(m("textarea", {
+        "onUpdate:modelValue": d[0] || (d[0] = (h) => l.value = h),
+        id: e.id,
+        class: a([e.inputClass + "__item"]),
+        placeholder: e.placeholder,
+        rows: e.rows,
+        onInput: i
+      }, null, 42, M), [
+        [S, l.value]
+      ])
+    ], 2));
+  }
+}, T = {
+  install: (e, t) => {
+    e.component("TextField", b), e.component("FormGroup", v), e.component("Label", p), e.component("Checkbox", q), e.component("ErrorAndHint", B), e.component("TextArea", w);
   }
 };
 export {
-  x as default
+  T as default
 };
